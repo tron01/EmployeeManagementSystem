7efacced81cb2b0b34f21644f4cb7ce626ca0fed
Abhijith-02 <69385258+tron01@users.noreply.github.com>
2025-04-10 16:16:27 +0530

    Employee Entity

--- src/main/java/com/Abhijith/EmployeeManagementSystem/Model/Employee.java
+++ src/main/java/com/Abhijith/EmployeeManagementSystem/Model/Employee.java
@@ -1,4 +1,98 @@
 package com.Abhijith.EmployeeManagementSystem.Model;
 
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+
+@Entity
 public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String name;
+    private Double salary;
+    private String address;
+    private String role;
+    private LocalDate joinDate;
+    private Double yearlyBonusPercentage;
+
+    @ManyToOne
+    @JoinColumn(name = "department_id")
+    private Department department;
+
+    @ManyToOne
+    @JoinColumn(name = "manager_id")
+    private Employee reportingManager;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public Double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(Double salary) {
+        this.salary = salary;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public LocalDate getJoinDate() {
+        return joinDate;
+    }
+
+    public void setJoinDate(LocalDate joinDate) {
+        this.joinDate = joinDate;
+    }
+
+    public Double getYearlyBonusPercentage() {
+        return yearlyBonusPercentage;
+    }
+
+    public void setYearlyBonusPercentage(Double yearlyBonusPercentage) {
+        this.yearlyBonusPercentage = yearlyBonusPercentage;
+    }
+
+    public Department getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(Department department) {
+        this.department = department;
+    }
+
+    public Employee getReportingManager() {
+        return reportingManager;
+    }
+
+    public void setReportingManager(Employee reportingManager) {
+        this.reportingManager = reportingManager;
+    }
 }
