69f0000c7f405c4912d31ab0be660838a7369238
Abhijith-02 <69385258+tron01@users.noreply.github.com>
2025-04-10 16:55:50 +0530

    Department Entity

--- src/main/java/com/Abhijith/EmployeeManagementSystem/Model/Department.java
+++ src/main/java/com/Abhijith/EmployeeManagementSystem/Model/Department.java
@@ -1,4 +1,63 @@
 package com.Abhijith.EmployeeManagementSystem.Model;
 
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
 public class Department {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String name;
+    private LocalDate creationDate;
+
+    @OneToOne
+    @JoinColumn(name = "head_id")
+    private Employee departmentHead;
+
+    @OneToMany(mappedBy = "department" ,cascade = CascadeType.ALL)
+    private List<Employee> employees =new ArrayList<>();
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public LocalDate getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(LocalDate creationDate) {
+        this.creationDate = creationDate;
+    }
+
+    public Employee getDepartmentHead() {
+        return departmentHead;
+    }
+
+    public void setDepartmentHead(Employee departmentHead) {
+        this.departmentHead = departmentHead;
+    }
+
+    public List<Employee> getEmployees() {
+        return employees;
+    }
+
+    public void setEmployees(List<Employee> employees) {
+        this.employees = employees;
+    }
 }
